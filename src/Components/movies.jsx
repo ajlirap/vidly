61f7d5460bae0b66be31b1d4c0f7d4b50163a4d8
ajlirap <ajlirap@gmail.com>
2021-09-03 08:22:22 -0700

    Vidly counters

--- src/Components/movies.jsx
+++ src/Components/movies.jsx
@@ -1,33 +1,54 @@
 import React, { Component } from "react";
-import { getMovies } from '../services/fakeMovieService';
+import { getMovies } from "../services/fakeMovieService";
 
 class Movies extends Component {
-    state = {
-        movies: getMovies()
-    };
-    render() {
-        return (
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Title</th>
-                        <th>Genre</th>
-                        <th>Stock</th>
-                        <th>Rate</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.state.movies.map(movie => (
-                        <tr>
-                            <td>{movie.title}</td>
-                            <td>{movie.genre.name}</td>
-                            <td>{movie.numberInStock}</td>
-                            <td>{movie.dailyRentalRate}</td>
-                        </tr> /
+  state = {
+    movies: getMovies(),
+  };
 
-                </tbody>
-            </table>
-        );
-    }
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id);
+    this.setState({ movies });
+    console.log(movie);
+  };
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p>There are no movies in the database.</p>;
+
+    return (
+      <React.Fragment>
+        <p>Showing {count} movies in the database.</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
 }
 export default Movies;
